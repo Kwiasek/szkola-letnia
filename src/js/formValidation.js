6a6cb5edbc8893311e5337ca3af9db943d4284b4
neyluu <75064225+neyluu@users.noreply.github.com>
2024-06-01 14:53:08 +0200

    Inputs max length + acceptable chars

--- src/js/formValidation.js
+++ src/js/formValidation.js
@@ -1,20 +1,52 @@
 function formValidation() {
     console.log("Form validation...");
 
-    // --- CONST VALUES ---
+    // --- SETTINGS ---
+    // ! Keys values in settings and errorFields should be identical to avoid problems with functions !
+    
     const invisible = "opacity-0";
     const visible = "opacity-100";
     const errorMessages = {
         emptyField: "Pole nie powinno być puste!",
         name: {
             digits: "Imie nie powinno zawierać cyfr!",
+            len: "Imie jest za długie!",
+            regex: "Imie zawiera niedozwolone znaki!",
         },
         surname: {
             digits: "Nazwisko nie powinno zawierać cyfr!",
+            len: "Nazwisko jest za długie!",
+            regex: "Nazwisko zawiera niedozwolone znaki!",
         },
-        email: "Email nie jest poprawny!",
+        email: {
+            regex: "Email nie jest poprawny!",
+            len: "Email jest za długi!",
+        },
+        nr: {
+            len: "Numer jest za długi!",
+        },
+        street: {
+            len: "Nazwa ulicy jest za długa!",
+        },
         zipcode: "Kod pocztowy nie jest poprawny!",
+        city: {
+            len: "Nazwa miejscowości jest za długa!",
+            regex: "Nazwa miejscowości zawiera niedozwolone znaki!",
+        },
     };
+    const maxLengths = {
+        name: 32,
+        surname: 64,
+        email: 128,
+        nr: 16,
+        street: 128,
+        city: 128,
+    };
+    const acceptedChars = {
+        name: " aąbcćdeęfghijklłmnńoópqrsśtuvwxyzźż-",
+        surname: " aąbcćdeęfghijklłmnńoópqrsśtuvwxyzźż-.",
+        city: " aąbcćdeęfghijklłmnńoópqrsśtuvwxyzźż-",
+    }
 
     // --- FORMS FIELDS --- 
     const nameField = document.querySelector("#name");
@@ -56,53 +88,74 @@ function formValidation() {
     validateAgeGroup();
     validateTerm();
 
+    // --- VALIDATION ---
     function validateName() {
-        let input = nameField.value;
+        let input = nameField.value.trim();
 
         if (isFieldEmpty(input, 'name')) return;
+        if (isInputTooLong(input, 'name')) return;
 
         for (const letter of input) {
             if (isDigit(letter)) {
                 isFormValid = false;
                 setError(errorsFields.name, errorMessages.name.digits);
+                return;
+            }
+
+            if (!acceptedChars.name.includes(letter.toLowerCase())) {
+                isFormValid = false;
+                setError(errorsFields.name, errorMessages.name.regex);
+                return;
             }
         }
     }
     function validateSurname() {
-        let input = surnameField.value;
+        let input = surnameField.value.trim();
 
         if (isFieldEmpty(input, 'surname')) return;
+        if (isInputTooLong(input, 'surname')) return;
 
         for (const letter of input) {
             if (isDigit(letter)) {
                 isFormValid = false;
                 setError(errorsFields.surname, errorMessages.surname.digits);
+                return;
+            }
+
+            if (!acceptedChars.surname.includes(letter.toLowerCase())) {
+                isFormValid = false;
+                setError(errorsFields.surname, errorMessages.surname.regex);
+                return;
             }
         }
     }
     function validateEmail() {
-        let input = emailField.value;
+        let input = emailField.value.trim();
         const validRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
         if (isFieldEmpty(input, 'email')) return;
+        if (isInputTooLong(input, 'email')) return;
 
         if (!input.match(validRegex)) {
             isFormValid = false;
-            setError(errorsFields.email, errorMessages.email);
+            setError(errorsFields.email, errorMessages.email.regex);
+            return;
         }
     }
     function validateHouseNr() {
-        let input = houseNrField.value;
+        let input = houseNrField.value.trim();
 
         if (isFieldEmpty(input, 'nr')) return;
+        if (isInputTooLong(input, 'nr')) return;
     }
     function validateStreet() {
-        let input = streetField.value;
+        let input = streetField.value.trim();
 
         if (isFieldEmpty(input, 'street')) return;
+        if (isInputTooLong(input, 'street')) return;
     }
     function validateZipCode() {
-        let input = zipCodeField.value;
+        let input = zipCodeField.value.trim();
         const validRegex = /^[0-9]{2}-[0-9]{3}$/;
 
         if (isFieldEmpty(input, 'zipcode')) return;
@@ -110,12 +163,22 @@ function formValidation() {
         if (!input.match(validRegex)) {
             isFormValid = false;
             setError(errorsFields.zipcode, errorMessages.zipcode);
+            return;
         }
     }
     function validateCity() {
-        let input = cityField.value;
+        let input = cityField.value.trim();
 
         if (isFieldEmpty(input, 'city')) return;
+        if (isInputTooLong(input, 'city')) return;
+
+        for (const letter of input) {
+            if (!acceptedChars.city.includes(letter.toLowerCase())) {
+                isFormValid = false;
+                setError(errorsFields.city, errorMessages.city.regex);
+                return;
+            }
+        }
     }
     function validateAgeGroup() {
         let input = ageGroupSelect.value;
@@ -123,6 +186,7 @@ function formValidation() {
         if (input === "empty") {
             isFormValid = false;
             setError(errorsFields.agegroup, errorMessages.emptyField);
+            return;
         }
     }
     function validateTerm() {
@@ -131,6 +195,7 @@ function formValidation() {
         if (input === "empty") {
             isFormValid = false;
             setError(errorsFields.term, errorMessages.emptyField);
+            return;
         }
     }
 
@@ -150,6 +215,15 @@ function formValidation() {
         if (value == null || value.trim().length == 0) {
             isFormValid = false;
             setError(errorsFields[errorField], errorMessages.emptyField);
+            return true;
+        }
+        return false;
+    }
+
+    function isInputTooLong(value, field) {
+        if (value.length > maxLengths[field]) {
+            isFormValid = false;
+            setError(errorsFields[field], errorMessages[field].len);
             return true;
         }
         return false;
