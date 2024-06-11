14830d9a562578219bbe335456606cb46652a099
neyluu <dwa2321@gmail.com>
2024-06-11 10:07:06 +0200

    Changed border color

--- src/components/project/Flashcard.jsx
+++ src/components/project/Flashcard.jsx
@@ -6,7 +6,7 @@ const Flashcard = (props) => {
     <div
       className={`info ${
         colors.bg[props.bgColor]
-      } rounded-xl p-3 text-justify border-2 border-coffee-800`}
+      } rounded-xl p-3 text-justify border-2 border-coffee-900`}
     >
       <p className="text-lg">{props.text}</p>
     </div>
